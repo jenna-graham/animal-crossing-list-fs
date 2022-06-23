c00b9212cdf846ae8adedec8dc6e6f1ed70b6fa6
jenna-graham <jclayto2@gmail.com>
2022-06-23 09:27:50 -0700

    added route to App.js and created list page link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import './App.css';
+import ListPage from './ListPage';
+import DetailPage from './DetailPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <NavLink exact activeClassName="active-link" to="/">List Page</NavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <ListPage />
+          </Route>
+          <Route exact path="/villagers/:id">
+            <DetailPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
