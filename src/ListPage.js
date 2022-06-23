36fa0c769e93166eebbab09ac2b25d81b54aa5df
jenna-graham <jclayto2@gmail.com>
2022-06-23 10:16:30 -0700

    mapped thru villager list

--- src/ListPage.js
+++ src/ListPage.js
@@ -17,7 +17,7 @@ export default function ListPage() {
   }
 
   useEffect(() => {
-      fetchVillagers();
+    fetchVillagers();
   });
 
     
--- src/VillagersList.js
+++ src/VillagersList.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Villager from './Villager';
 
-export default function VillagersList() {
+export default function VillagersList({ villagers }) {
+  console.log(villagers);
   return (
-    <div>VillagersList</div>
+    <div>
+      { 
+        villagers.map((villager, i) => {
+          return <Villager {...villager} key={villager.UniqueEntryID + i} />;
+        })
+      }
+    </div>
   );
 }
