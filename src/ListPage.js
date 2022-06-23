09044a85be35ed26bf80fb9724c51563c5651cd9
jenna-graham <jclayto2@gmail.com>
2022-06-23 09:55:24 -0700

    useEffect to fetch villagers

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { useState, } from 'react';
+import { useState, useEffect } from 'react';
 import { getVillagersData } from './services/fetch-utils';
 import VillagersList from './VillagersList';
 
 export default function ListPage() {
   const [villagers, setVillagers] = useState([]);
-  const [totalVillagers, settotalVillagers] = useState(1);
   const [page, setPage] = useState(1);
   const perPage = 25;
 
-  
+  async function fetchVillagers() {
+    const from = page * perPage - perPage;
+    const to = page * perPage;
+    const villagers = await getVillagersData(from, to);
+
+    setVillagers(villagers);
+  }
+
+  useEffect(() => {
+      fetchVillagers();
+  });
 
     
   return (
@@ -19,7 +28,7 @@ export default function ListPage() {
         <button disabled={page <= 1} onClick={() => setPage(page - 1)}>Previous Page</button>
         <button onClick={() => setPage(page + 1)}>Next Page</button>
       </div>
-      <VillagersList />
+      <VillagersList villagers={villagers} />
         
     </div>
   );
